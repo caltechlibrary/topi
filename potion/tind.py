a322e8bb7b6e1225fba84c3cf826cddddb3ff34c
Michael Hucka <mhucka@caltech.edu>
2021-02-16 12:06:20 -0800

    Splitting out code to handle Item and Record

--- potion/tind.py
+++ potion/tind.py
@@ -1,6 +1,12 @@
 '''
 tind.py: code for interacting with a TIND server
 
+This downloads data in the MARC XML format instead of other formats, because
+it contains data not present in the other output formats.  In addition,
+TIND doesn't seem to do any additional cleanup on the data in the other
+formats, so there's no real advantage. E.g., I find title values in the other
+formats ending with "/", some fields have trailing commas in their values, etc.
+
 Authors
 -------
 
@@ -18,13 +24,15 @@ from   collections import namedtuple
 from   commonpy.interrupt import wait
 from   commonpy.network_utils import net
 from   commonpy.exceptions import NoContent, ServiceFailure, RateLimitExceeded
-import json as jsonlib
+import json
+from   json import JSONDecodeError
 from   lxml import etree, html
 
 if __debug__:
     from sidetrack import log
 
 from .exceptions import *
+from .item import Item
 from .thumbnail_utils import thumbnail_from_google, thumbnail_from_amazon
 from .record import Record
 
@@ -38,11 +46,16 @@ ELEM_CONTROLFIELD = '{http://www.loc.gov/MARC21/slim}controlfield'
 ELEM_DATAFIELD    = '{http://www.loc.gov/MARC21/slim}datafield'
 ELEM_SUBFIELD     = '{http://www.loc.gov/MARC21/slim}subfield'
 
-# URL templates for retrieving data from a TIND server.
+# URL templates for retrieving a record from a TIND server.
 # The first placeholder is for the host URL; the second is for an identifier.
 # Use Python .format() to substitute the relevant values into the string.
 _XML_USING_BARCODE = '{}/search?p=barcode%3A+{}&of=xm'
 _XML_USING_TIND_ID = '{}/search?recid={}&of=xm'
+
+# URL templates for item data from a TIND server.
+# The first placeholder is for the host URL; the second is for a TIND record id.
+# Use Python .format() to substitute the relevant values into the string.
+_JSON_USING_TIND_ID = '{}/nanna/bibcirc/{}/details'
 
 # Time in seconds we pause if we hit the rate limit, and number of times we
 # repeatedly wait before we give up entirely.
@@ -60,33 +73,105 @@ class Tind():
         self.server_url = server_url
 
 
-    def record(self, barcode = None, tind_id = None, marc_xml = None):
-        '''Return a Record object from TIND constructed from MARC XML data.
+    def record(self, tind_id = None, marc_xml = None):
+        '''Create a Record object given either a TIND id or MARC XML from TIND.
 
-        Keword arguments "barcode", "tind_id" and "marc_xml" are mutually
-        exclusive.
+        Keyword arguments "tind_id" and "marc_xml" are mutually exclusive.
 
-        If either "barcode" or "tind_id" is given, this will contact the TIND
-        server, look up the record using either the barcode or the TIND id
-        (depending on which one was given), download the MARC XML form of the
-        TIND item data, and create the record based on data found.
+        If "tind_id" is given, this will search the TIND server using the id,
+        download the MARC XML returned by TIND, create a Record object based
+        on the data, then separately, also use TIND's Item record API to find
+        all the items/holdings for the record, create corresponding Item
+        objects for each, and add them to the "items" list within the Record
+        object. If the TIND server does not return a result for the id, this
+        method raises a potion.NotFound exception.
 
-        If "marc_xml" is given instead, this will create the record using
-        the given XML string.  The XML must have been obtained using the MARC
-        XML export feature of TIND.  Using "marc_xml" will not result in the
-        TIND server being contacted.
+        If "marc_xml" is given, the XML must have been obtained using the
+        MARC XML export feature of TIND.  This method will skip the
+        preliminary TIND search and instead, create the record using the
+        given XML string, then perform the same steps as described above for
+        the case of tind_id.
+
+        If neither "tind_id" nor "marc_xml" is given, this method returns an
+        empty Record object.
         '''
-        args = locals().copy()
-        del args['self']
-        given_keywords = list(keyword for keyword in args if args[keyword])
-        if len(given_keywords) == 1:
-            given = given_keywords[0]
-            return self._record_from_argument[given](self, args[given])
-        elif len(given_keywords) == 0:
-            raise ValueError(f"At least one argument required, but none given")
-        else:                           # More than one keyword arg given.
-            given = ', '.join(given)
-            raise ValueError(f"Conflicting arguments given: {given}")
+        if tind_id and marc_xml:
+            raise ValueError(f'"tind_id" and "marc_xml" are mutually exclusive.')
+
+        if tind_id:
+            tind_id = str(tind_id)
+            if not tind_id.isdigit():
+                raise ValueError(f'Invalid argument: {tind_id} is not a number.')
+            record = self._record_from_server(_XML_USING_TIND_ID, tind_id)
+        elif marc_xml:
+            if not marc_xml.startswith(b'<?xml'):
+                raise ValueError(f'marc_xml argument does not appear to be XML.')
+            record = self._record_from_xml(marc_xml)
+        else:
+            return Record()
+
+        if record:
+            record.items = self._items_for_tind_id(tind_id or record.tind_id)
+            for item in record.items:
+                item.parent = record
+            record.thumbnail = self._thumbnail_for_tind_id(record.tind_id)
+            return record
+        else:
+            arg = tind_id if tind_id else 'given XML data'
+            raise NotFound(f'No record found for {arg} in {self.server_url}')
+
+
+    def item(self, barcode = None):
+        '''Create an Item record given a barcode value.
+
+        This will contact the TIND server and perform a search using the
+        barcode value, then using the data returned, create a Record object
+        with an Item object within it, and finally return the Item object.
+        (Item objects contain a property referencing their parent Record
+        objects, so callers can get the Record given the Item object.
+
+        If the TIND server does not return a result for the barcode, this
+        method raises a potion.NotFound exception
+
+        If no barcode is given, this returns an empty Item object.
+        '''
+        if not barcode:
+            return Item()
+        barcode = str(barcode)
+        if not barcode.isdigit():
+            raise ValueError(f'Invalid argument: {barcode} is not a number.')
+        record = self._record_from_server(_XML_USING_BARCODE, barcode)
+        if record:
+            record.items = self._items_for_tind_id(record.tind_id)
+            for item in record.items:
+                item.parent = record
+            for item in record.items:
+                if item.barcode == barcode:
+                    return item
+            raise PotionError('Record-Item mismatch -- please report this error.')
+        else:
+            raise NotFound(f'No record found for {barcode} in {self.server_url}')
+
+
+    def _record_from_server(self, search_template, id, retry = 0):
+        (resp, error) = net('get', search_template.format(self.server_url, id))
+        if not error:
+            if __debug__: log(f'got result from {self.server_url} for {id}')
+            return self._record_from_xml(resp.content)
+        elif isinstance(error, NoContent):
+            if __debug__: log(f'got empty xml content for {id}')
+            return Record()
+        elif isinstance(error, RateLimitExceeded):
+            retry += 1
+            if retry > _MAX_SLEEP_CYCLES:
+                raise TindError(f'Rate limit exceeded for {self.server_url}')
+            else:
+                if __debug__: log(f'hit rate limit; pausing {_RATE_LIMIT_SLEEP}s')
+                wait(_RATE_LIMIT_SLEEP)
+                return self._record_from_server(search_template, id, retry = retry)
+        else:
+            if __debug__: log(f'got {type(error)} error for {id}')
+            raise TindError(f'Problem contacting {self.server_url}: {str(error)}')
 
 
     def _record_from_xml(self, xml):
@@ -122,12 +207,6 @@ class Tind():
                 record.call_no = ''
                 for subfield in element.findall(ELEM_SUBFIELD):
                     record.call_no += subfield.text.strip() + ' '
-            elif element.attrib['tag'] == '090':
-                # If the record has call numbers in both 050 and 090, the 090
-                # value will overwrite the 050 one, and that's what we want.
-                record.call_no = ''
-                for subfield in element.findall(ELEM_SUBFIELD):
-                    record.call_no += subfield.text.strip() + ' '
             elif element.attrib['tag'] == '100':
                 for subfield in element.findall(ELEM_SUBFIELD):
                     if subfield.attrib['code'] == 'a':
@@ -147,7 +226,7 @@ class Tind():
                     # Value is sometimes of the form "1429224045 (hbk.)"
                     value = subfield.text.split()[0]
                     if value.isdigit():
-                        record.isbn_list.append(value)
+                        record.isbn_issn.append(value)
 
         # We get author from 245 because in our entries, it's frequently part
         # of the title statement. If it's not, but we got an author from 100
@@ -162,7 +241,7 @@ class Tind():
 
         # Caltech's TIND database contains some things that are not reading
         # materials per se. The following is an attempt to weed those out.
-        if sum([not record.author, not record.year, not record.call_no]) > 1:
+        if sum([not record.author, not record.year, not record.title]) > 1:
             for field in ['title', 'author', 'year', 'call_no', 'edition']:
                 setattr(record. field, None)
             return record
@@ -171,53 +250,62 @@ class Tind():
         record.author  = cleaned(record.author)
         record.title   = cleaned(record.title)
         record.edition = cleaned(record.edition)
-        record.call_no = cleaned(record.call_no)
         if subtitle:
             # A separate subtitle is not useful for us, so merge it into title.
             if subtitle.find('/') > 0:
                 subtitle = subtitle[:-1].strip()
             record.title += ': ' + subtitle
 
-        # Get other items.
-        # self._get_thumbnail()
+        record.url = f'{self.server_url}/record/{record.tind_id}'
         return record
 
 
-    def _record_from_barcode(self, barcode):
-        record = self._record_from_id(_XML_USING_BARCODE, barcode)
-        record.barcode = str(barcode)
-        return record
-
-
-    def _record_from_tind_id(self, tind_id):
-        return self._record_from_id(_XML_USING_TIND_ID, tind_id)
-
-
-    def _record_from_id(self, endpoint_template, id, retry = 0):
-        (resp, error) = net('get', endpoint_template.format(self.server_url, id))
+    # Fixme: rename that json template
+    def _items_for_tind_id(self, id, retry = 0):
+        (resp, error) = net('get', _JSON_USING_TIND_ID.format(self.server_url, id))
+        results = []
         if not error:
             if __debug__: log(f'got result from {self.server_url} for {id}')
-            return self._record_from_xml(resp.content)
+            try:
+                data = json.loads(resp.text)
+            except JSONDecodeError as ex:
+                raise TindError(f'Malformed result from {self.server_url}: str(ex)')
+            except TypeError as ex:
+                raise PotionError('Error during item lookup -- please report this.')
+
+            if 'items' not in data:
+                if __debug__: log(f'results from server missing "data" key')
+                raise TindError(f'Unexpected result from {self.server_url}')
+            for item in data['items']:
+                results.append(Item(barcode     = item.get('barcode', ''),
+                                    type        = item.get('item_type', ''),
+                                    volume      = item.get('item_volume', ''),
+                                    call_number = item.get('call_number', ''),
+                                    description = item.get('description', ''),
+                                    library     = item.get('library', '',),
+                                    location    = item.get('location', ''),
+                                    status      = item.get('status', '')))
+            return results
         elif isinstance(error, NoContent):
-            if __debug__: log(f'got empty content for {id}')
-            return Record()
+            if __debug__: log(f'got empty json content for {id}')
+            return []
         elif isinstance(error, RateLimitExceeded):
             retry += 1
             if retry > _MAX_SLEEP_CYCLES:
-                raise ServerError(f'Rate limit exceeded for {self.server_url}')
+                raise TindError(f'Rate limit exceeded for {self.server_url}')
             else:
                 if __debug__: log(f'hit rate limit; pausing {_RATE_LIMIT_SLEEP}s')
                 wait(_RATE_LIMIT_SLEEP)
-                return self._record_from_id(endpoint_template, id, retry = retry)
+                return self._items_for_tind_id(id, retry = retry)
         else:
             if __debug__: log(f'got {type(error)} error for {id}')
-            raise ServerError(f'Problem contacting {self.server_url}: {str(error)}')
+            raise TindError(f'Problem contacting {self.server_url}: {str(error)}')
 
 
-    # Initialize the dispatcher after defining the methods themselves.
-    _record_from_argument = {'barcode' : staticmethod(_record_from_barcode).__func__,
-                             'tind_id' : staticmethod(_record_from_tind_id).__func__,
-                             'marc_xml': staticmethod(_record_from_xml).__func__}
+
+    def _thumbnail_for_tind_id(self, tind_id):
+        pass
+
 
 
 # Miscellaneous helpers.
